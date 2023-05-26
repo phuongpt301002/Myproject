b065358760b5fa59c3801f12ccbf5e269ca4848a
PUONii <93080428+phuongpt301002@users.noreply.github.com>
2023-05-26 21:07:04 +0700

    Delete cntt62_3/BuilderPattern directory

--- cntt62_3/BuilderPattern/Bai3/ShapeType.java
+++
@@ -1,4 +0,0 @@
-package creational.factory.cntt62_3.BuilderPattern.Bai3;
-
-public enum ShapeType {
-}
--- cntt62_3/BuilderPattern/Bai6/UI2.java
+++
@@ -1,6 +0,0 @@
-package creational.factory.cntt62_3.BuilderPattern.Bai6;
-
-public class UI2 {
-    //Lấy 1 đối tượng data Access: getInstance
-    //Gọi phương thức add trên đôi tượng vừa lấy
-}
